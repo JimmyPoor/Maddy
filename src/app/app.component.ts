c6be2a4bfac5570658e398ac5c6173759fd169e0
huachaoZheng <kissnana5@live.cn>
2017-12-26 23:46:06 +0800

    angular first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
+
+  @Input()
+  public alerts: Array<IAlert> = [];
+
+  constructor() {
+    this.alerts.push({
+      id: 1,
+      type: 'success',
+      message: 'This is an success alert',
+    }, {
+        id: 2,
+        type: 'info',
+        message: 'This is an info alert',
+      });
+  }
+
+  closeAlert(alert: IAlert): void {
+    const index: number = this.alerts.indexOf(alert);
+    this.alerts.splice(index, 1);
+  }
+}
+
+
+export interface IAlert {
+  id: number;
+  type: string;
+  message: string;
 }
